1ff7f416297554cbf165df34e72093bb1770ebe7
Dong-Young Kim <31337.persona@gmail.com>
2020-09-27 18:45:31 +0900

    fix(app.jsx): update app baseline

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
+const theme = createMuiTheme();
 const App = () => {
-  return <div></div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+    </ThemeProvider>
+  );
 };
 
 export default App;
