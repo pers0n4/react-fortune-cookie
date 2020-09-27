e31299f27cf228d7820c2d7fbe29f8dfbb613b24
Dong-Young Kim <31337.persona@gmail.com>
2020-09-27 21:40:21 +0900

    feat(footer): add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import {
   ThemeProvider,
@@ -9,6 +9,7 @@ import {
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 
+import Footer from "./Footer";
 import Cookie from "./Cookie";
 
 const theme = createMuiTheme();
@@ -32,16 +33,14 @@ const App = () => {
   const classes = useStyles();
 
   const [message, setMessage] = useState("");
-  const [messages, setMessages] = useState([
-    "완벽보다 완성을 추구하라",
-    "내가 알고 있는 단 하나는 내가 모른다는 사실뿐",
-    "읽었다면 써라, 들었다면 말하라, 배웠다면 가르쳐라",
-    "사람이 고통을 대하는 두 가지 방식은 극복하거나, 답습하거나",
-    "감정의 문은 하나이기에 상처가 두려워 문을 닫으면 행복 또한 마주할 수 없다",
-    "거짓이 지은 웃음보다 진실이 주는 고통을",
-    "선악은 없고 각자의 정의만이 남아있으리",
-    "모든 삶은 죽음이라는 완성을 향해가는 미완성이다",
-  ]);
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const data = await fetch("/messages.json");
+      setMessages(await data.json());
+    })();
+  }, []);
 
   const handleCookieClick = () => {
     const randomIndex = Math.floor(Math.random() * messages.length);
@@ -56,6 +55,7 @@ const App = () => {
           <Cookie onClick={handleCookieClick} />
           <Typography>{message}</Typography>
         </Container>
+        <Footer />
       </div>
     </ThemeProvider>
   );
---
+++ src/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Container from "@material-ui/core/Container";
+
+const Footer = () => {
+  return (
+    <Container component="footer" maxWidth="md">
+      <p>
+        Icons made by{" "}
+        <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+          Freepik
+        </a>{" "}
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </p>
+    </Container>
+  );
+};
+
+export default Footer;
